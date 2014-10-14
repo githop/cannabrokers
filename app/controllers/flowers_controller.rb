366e524003a6f4f8a8b2c49eff72fed68b502439
Tom Hopkins <tom@githop.com>
2014-10-14 14:19:22 -0600

    flowers can be created from new view, no validations

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -13,4 +13,22 @@ class FlowersController < ApplicationController
 		authorize Flower
 		@flower = Flower.new
 	end
+
+	def create
+		@flower = Flower.new
+		@flower.strain = flower_params[:strain]
+		@flower.kind   = flower_params[:kind]
+		@flower.description = flower_params[:description]
+		@flower.quantity_price = flower_params[:quantity_price]
+		@flower.merchants << Merchant.find_by_name(flower_params[:merchants])
+		if @flower.save
+			redirect_to @flower
+		end
+	end
+
+	private
+
+	def flower_params
+		params.require(:flower).permit(:strain, :kind, :description, :quantity_price, :merchants)
+	end
 end
--- app/policies/flower_policy.rb
+++ app/policies/flower_policy.rb
@@ -9,4 +9,8 @@ class FlowerPolicy < ApplicationPolicy
 	def new?
 		@current_user.admin?
 	end
+
+	def create?
+		@current_user.admin?
+	end
 end
