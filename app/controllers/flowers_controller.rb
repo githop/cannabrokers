9384ac5827340f5414af3b05b77c4f3da159fc2c
Tom Hopkins <tom@githop.com>
2014-10-17 15:20:59 -0600

    figured out how to allow edit without creating a new merchant assoc due to default val in dropdown

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -1,5 +1,5 @@
 class FlowersController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
 	def index
 		@flowers = Flower.all
@@ -16,20 +16,44 @@ class FlowersController < ApplicationController
 
 	def create
 		authorize Flower
-		@flower = Flower.new
-		@flower.strain = flower_params[:strain]
-		@flower.kind   = flower_params[:kind]
-		@flower.description = flower_params[:description]
-		@flower.quantity_price = flower_params[:quantity_price]
+		@flower = Flower.new(flower_params)
 		if @flower.save
-			@flower.merchants << Merchant.find_by_name(flower_params[:merchants])
+			get_merchant(@flower)
 			redirect_to @flower
+		end
+	end
+
+	def edit
+		authorize Flower
+		@flower = Flower.find(params[:id])
+		@merchant = @flower.merchants
+	end
+
+	def update
+		@flower = Flower.find(params[:id])
+		if @flower.update(flower_params)
+			get_merchant(@flower)
+			redirect_to @flower
+		else
+			render 'edit'
 		end
 	end
 
 	private
 
 	def flower_params
-		params.require(:flower).permit(:strain, :kind, :description, :quantity_price, :merchants)
+		params.require(:flower).permit(:strain, :kind, :description, :quantity_price)
 	end
+
+	def merch_param
+		params.require(:flower).permit(:merchants)
+	end
+
+	def get_merchant(flower)
+		if !merch_param[:merchants].blank?
+			merch = Merchant.find(merch_param[:merchants])
+			flower.merchants << merch
+		end
+	end
+
 end
