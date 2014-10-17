de79d3bedd4e0bef7d3dc4ee2530885cab557441
Tom Hopkins <tom@githop.com>
2014-10-17 13:16:45 -0600

    adds merchant new and create controller actions and views

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -11,4 +11,21 @@ class MerchantsController < ApplicationController
 	def concentrates
 		@merchants = Merchant.includes(:concentrates).order('premium DESC').each_with_object([]) {|m, a| m.concentrates.any? ? a << m : false }
 	end
+
+	def new
+		@merchant = Merchant.new
+	end
+
+	def create
+		@merchant = Merchant.new(merch_params)
+		if @merchant.save
+			redirect_to merchant_path(@merchant)
+		end
+	end
+
+	private
+
+	def merch_params
+		params.require(:merchant).permit(:name, :description, :phone, :email, :premium)
+	end
 end
