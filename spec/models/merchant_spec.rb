54b5fd036edcdede1602fbf0af269d932742091e
Tom Hopkins <tom@githop.com>
2014-10-18 19:32:12 -0600

    adds specs for concentrate, edible, flower, and merchant models

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,61 +1,53 @@
 require 'rails_helper'
 
+
 RSpec.describe Merchant, type: :model do 
-	let(:merchant) { Merchant.create!(name: "Boulder Botanics", description: "this is it") }
 
-	describe "the merchant model " do 
-		context "in terms of associations " do 
-			it "should have many flowers." do 
-				merch = Merchant.reflect_on_association(:edibles)
-				expect merch.macro == :has_many
+	describe "the merchant model " do
+		context "basic validations " do 
+			it "has a valid factory" do 
+				merchant = FactoryGirl.build(:merchant)
+				expect(merchant).to be_valid
 			end
 
-			it "should have many concentrates." do 
-				merch = Merchant.reflect_on_association(:concentrates)
-				expect merch.macro == :has_many
+			it "should require a name" do 
+				no_name = FactoryGirl.build(:merchant, name: nil)
+				expect(no_name).to_not be_valid
 			end
 
-			it "should have many edibles " do
-				merch = Merchant.reflect_on_association(:edibles) 
-				expect merch.macro == :has_many
+			it "should require a phone " do 
+				no_phone = FactoryGirl.build(:merchant, phone: nil)
+				expect(no_phone).to_not be_valid
 			end
 
-			it "should have a join table called inventories" do 
-				merch = Merchant.reflect_on_association(:inventories)
-				expect merch.macro == :has_many
+			it "should require an email" do 
+				no_email = FactoryGirl.build(:merchant, email: nil)
+				expect(no_email).to_not be_valid
+			end
+
+			it "should require a well formatted email" do 
+				bad_email = FactoryGirl.build(:merchant, email: "derpy.com")
+				expect(bad_email).to_not be_valid
 			end
 		end
 		
-		context "class methods " do 
+		context "class methods " do
+
 			describe "the :edible_companies method" do
 				it "should return a list of only edible companies" do
+					non_edible_comp = FactoryGirl.build(:merchant)
 					edible_comps = Merchant.edible_companies
 
-					def check_comps(comps)
-						count = comps.count
-						check = comps.keep_if do |comp|
-							comp.edibles.any?
-						end
-						check.count == count
-					end
-
-					expect check_comps(edible_comps) == true
+					expect(edible_comps.include?(non_edible_comp)).to eq(false)
 				end
 			end
 
 			describe "the :concentrate_companies method" do 
-				it "should return a list of only concentrate companies" do 
+				it "should return a list of only concentrate companies" do
+					non_concentrate = FactoryGirl.build(:merchant)
 					concentrate_comps = Merchant.concentrate_companies
 
-					def check_comps(comps)
-						count = comps.count
-						check = comps.keep_if do |comp|
-							comp.concentrates.any?
-						end
-						check.count == count
-					end
-
-					expect check_comps(concentrate_comps) == true
+					expect(concentrate_comps.include?(non_concentrate)).to eq(false)
 				end
 			end
 		end 
