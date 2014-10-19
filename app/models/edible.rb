4c6a744b254d92d203ceb1e54a1692a9e6e933ed
Tom Hopkins <tom@githop.com>
2014-10-18 19:23:11 -0600

    adds validations

--- app/models/edible.rb
+++ app/models/edible.rb
@@ -1,4 +1,8 @@
 class Edible < ActiveRecord::Base
 	has_many :inventories
 	has_many :merchants, through: :inventories
+
+	validates :name, presence: true
+	validates :description, presence: true
+	validates :price, presence: true
 end
