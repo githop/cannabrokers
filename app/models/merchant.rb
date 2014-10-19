8d98f7090016a45e0963545de2a29b72165e1f49
Tom Hopkins <tom@githop.com>
2014-10-18 19:22:45 -0600

    adds validations and class methods to return either edible or concentrate companies

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,11 +4,16 @@ class Merchant < ActiveRecord::Base
 	has_many :concentrates, through: :inventories
 	has_many :edibles, through: :inventories
 
+	validates :name, presence: true
+	validates :phone, presence: true
+	validates :email, presence: true
+	validates :email, format: { with: /.+@.+\..+/i, message: "improper format" }
+
 	def self.edible_companies
-		merchants = Merchant.includes(:edibles).order('premium DESC').each_with_object([]) {|m, a| m.edibles.any? ? a << m : false }
+		self.includes(:edibles).order('premium DESC').each_with_object([]) {|m, a| m.edibles.any? ? a << m : false }
 	end
 
 	def self.concentrate_companies
-		merchants = Merchant.includes(:concentrates).order('premium DESC').each_with_object([]) {|m, a| m.concentrates.any? ? a << m : false }
+		self.includes(:concentrates).order('premium DESC').each_with_object([]) {|m, a| m.concentrates.any? ? a << m : false }
 	end
 end
