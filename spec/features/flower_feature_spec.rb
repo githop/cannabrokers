f86a2bc931baf67bbf43c1bdf26e2447994f6bfb
Tom Hopkins <tom@githop.com>
2014-11-03 11:43:21 -0700

    currently struggling with flower spec, specifically capybara option select

---
+++ spec/features/flower_feature_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+
+feature 'the flower resource' do
+	before :each do
+		@admin = FactoryGirl.create(:admin)
+		visit('/admin')
+			fill_in 'user[email]', with: @admin.email
+			fill_in 'user[password]', with: @admin.password
+			click_button("Log in")
+	end
+
+	context 'As admin' do
+			it "can add a new flower from the panel" do
+			visit('/admin')
+			click_link("Add a new Flower")
+			within('#new_flower') do
+				fill_in("Strain", with: "Capybara")
+				select("Hybrid", from: "flower_kind")
+				fill_in("Quantity / Price", with: "$50" )
+				click_button("Create Flower")
+			end
+			expect(page).to have_content("Capybara")
+		end
+	end
+end
