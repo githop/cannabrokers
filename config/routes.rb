75fe3af39bd511b1f4f68321473169ab23417d6e
Tom Hopkins <tom@githop.com>
2014-12-06 10:37:42 -0700

    adds flowers/all route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'merchants/edibles' => 'merchants#edibles'
   get 'merchants/concentrates' => 'merchants#concentrates'
+  get 'flowers/all' => 'flowers#all'
   get 'admin' => 'admin#panel'
 
 
